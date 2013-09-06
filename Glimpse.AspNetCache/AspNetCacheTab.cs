ea62cad6774126335057c9d9fc2490bc3409db33
Mathieu Lafontaine <mathieulafontaine@hotmail.com>
2013-09-06 14:50:47 -0400

    Initial commit

---
+++ Glimpse.AspNetCache/AspNetCacheTab.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glimpse.Core.Extensibility;
+using Glimpse.AspNet.Extensions;
+using Glimpse.AspNet.Extensibility;
+using System.Security.Principal;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Glimpse.AspNetCache
+{
+    public class AspNetCacheTab : TabBase<HttpContextBase>
+    {
+        public override string Name
+        {
+            get
+            {
+                return "ASP.NET Cache";
+            }
+        }
+
+        public string Key
+        {
+            get
+            {
+                return "glimpse_aspnet_cache";
+            }
+        }
+
+        public override object GetData(ITabContext context)
+        {
+            var cache = context.GetHttpContext().Cache;
+
+            return cache.Cast<DictionaryEntry>().Select(x => new { Key = x.Key, Type = x.Value.GetType().ToString(), Value = x.Value });
+        }
+    }
+}
---
+++ Glimpse.AspNetCache/Package.bat
@@ -0,0 +1,1 @@
+nuget.exe pack Glimpse.AspNetCache.csproj
